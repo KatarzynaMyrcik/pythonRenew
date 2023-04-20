a88196762c13d5269c54f8fe8e6a24f8045bf78a
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-20 16:14:30 +0200

    Create math_expressions.py

---
+++ math_expressions.py
@@ -0,0 +1,22 @@
+#1
+print (10 + 4)
+#2
+print(10/3)
+#the result is a float
+#3
+print(10//3)
+#the result is an integer
+#4
+print(10 % 3)
+#the result is the 'reszta' of division
+#5
+print(10 ** 3)
+#the result is 10 to the power of 3
+#6
+x = 10
+#addition of 3
+x = x + 3
+print(x)
+#another addtion of 3
+x += 3
+print(x)
