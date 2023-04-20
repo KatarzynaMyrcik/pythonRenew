b6dc3509c03daa2e24621b73980b44937da9a8d0
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-20 15:08:57 +0200

    test of knowledge about variables

---
+++ test_weight.py
@@ -0,0 +1,5 @@
+weight = input('Give me please your weight in kg: ')
+#1 pound it is 0.45359237 kg
+#conversion
+pdweight = float(weight) / 0.45
+print(str(round(pdweight, 1)) + '  is your weight in pounds')
