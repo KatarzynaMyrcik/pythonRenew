7dba1c1702d2435c67623d4766ea7883a148c681
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-21 12:20:55 +0200

    update with input, if, and change of uppercase to lowercase usinf [string].lower() fonction

--- test_weight.py
+++ test_weight.py
@@ -1,5 +1,16 @@
-weight = input('Give me please your weight in kg: ')
+weight = input('Give me please your weight: ')
+type = input("Could you please write if it is a weight in kg or pounds? For kg please type [K], for pounds please type [P] ")
 #1 pound it is 0.45359237 kg
-#conversion
-pdweight = float(weight) / 0.45
-print(str(round(pdweight, 1)) + '  is your weight in pounds')
+
+#print(type)
+low_type = type.lower()
+#print(low_type)
+
+if low_type == 'p':
+    pdweight = float(weight) / 0.45
+    print(str(round(pdweight, 1)) + '  is your weight in kg')
+elif low_type == 'k':
+    pdweight = float(weight) * 0.45
+    print(str(round(pdweight, 1)) + '  is your weight in pounds')
+else:
+    print('hmmm. Something went wrong. Maybe it was your cat clicking on the keyboard...')
