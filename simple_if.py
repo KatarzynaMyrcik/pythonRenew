909ad04ef336159384c84ca6c0a1afd50d7a5519
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-21 10:55:03 +0200

    this is a simple if test

---
+++ simple_if.py
@@ -0,0 +1,9 @@
+good_credit = True
+cost = 1000000
+
+if good_credit:
+    down_payment = 0.1 * cost
+else:
+    down_payment = 0.2 * cost
+
+print(f'the down payment of house will be {down_payment} euro')
