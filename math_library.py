213b1a3ddcba5452c5ad28280a2450f8001399d7
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-21 10:34:45 +0200

    I've tested math module

---
+++ math_library.py
@@ -0,0 +1,15 @@
+#it is good to import math module when you want to do maths calculations
+import math
+#when you want to use the math method you have to do it like it: match.[fonction]. There is a documentation of math:https://docs.python.org/3/library/math.html
+#For exemple:
+#It will round up the number:
+print(math.ceil(2.9))
+#it will round down the number:
+print(math.floor(2.9))
+##########################################
+x = 2.9
+#how to round w number without math library
+print(round(x))
+#how to get absolute value without math library
+print(abs(-2.9))
+#it is good to import math module when you want to do maths calculations.
