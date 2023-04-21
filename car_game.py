47fc9ccf99735bf9cd0c8caad2d6a6cf8618f92b
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-21 14:18:09 +0200

    simple car game

---
+++ car_game.py
@@ -0,0 +1,20 @@
+#game Car
+print('Welcome in my car game!')
+print("you can start. If you don't know the options please type [h]elp.")
+print("What do you want to do? ")
+action = ' '
+while action != 'q':
+    action = input('> ').lower()
+    if action == 'h':
+        print("""You choose [h]elp
+        In this game You have a possibility to:
+        [start] a car
+        [stop] a car
+        [q]uit the game
+        What do you want to do? """)
+    elif action == 'start':
+        print('the car is starting')
+    elif action == 'stop':
+        print('the car is stopped')
+else:
+    print("OK. Thank you! Have a nice day!")
