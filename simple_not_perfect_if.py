b72514cace588bade1e5c260660b98beb9346509
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-21 10:44:43 +0200

    it is simple not perfect if use
    (in case of 2 true it is not logical)

---
+++ simple_not_perfect_if.py
@@ -0,0 +1,11 @@
+is_hot = True
+is_cold = False
+
+if is_hot:
+    print('It is a hot day. Drink a lot of water')
+elif is_cold:
+    print("It's a cold day")
+    print("wear a warm clothes")
+else:
+    print('it is a lovely day')
+print("enjou your day")
