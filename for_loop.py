f9ef973bfc1859f2f2b346a4641bcdbb8c893d2d
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-21 15:55:20 +0200

    first for loop

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+#the begginings of for loop
+#string
+for item in 'Python':
+    print(item)
+#list
+for item in ['Ban', 'Big', 'Bom']:
+    print(item)
+#range fonction
+for item in  range(10):
+    print(item)
+
+for item in range(5, 10, 2):
+    print(item)
