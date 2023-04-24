81fc8799fdf8a86df3e72bd29a012c20ef5d04c7
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-24 12:19:15 +0200

    nested loop

---
+++ nested_loop_1.py
@@ -0,0 +1,18 @@
+#nested loops. one loop in another loop.
+for x in range(4):
+    for y in range(3):
+        print(f'({x},{y})')
+'''The result is:
+(0,0)
+(0,1)
+(0,2)
+(1,0)
+(1,1)
+(1,2)
+(2,0)
+(2,1)
+(2,2)
+(3,0)
+(3,1)
+(3,2)
+'''
