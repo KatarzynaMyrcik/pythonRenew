f3a663e17297b16a2dfd75b0ade788152c95e537
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-24 15:19:11 +0200

    tuples and 2 possible methods for tuples

---
+++ testing_tuples.py
@@ -0,0 +1,7 @@
+#tuples - they are not changebles insteaof [] we use ()
+numbers = (1,2,5,2,7)
+print(numbers[1])
+# to know how often in tuple a number 2 is present:
+print(numbers.count(2))
+#to know which index has a number:
+print(numbers.index(2))
