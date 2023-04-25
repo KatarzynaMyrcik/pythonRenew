c3318a07575c4f8802c80846c72050b338d0396a
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-25 11:54:50 +0200

    greeting foction which takes 2 arguments

---
+++ greeting_fonction_2.py
@@ -0,0 +1,9 @@
+def greetings(first_name, last_name):
+    print(f"""HI {first_name} {last_name}!
+Welcome aboard""")
+
+
+print("start")
+greetings("John", "Jajo")
+greetings("Mary", "klaczek")
+print("finish")
