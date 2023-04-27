eebc5fe5677eda91279d354e7bbf06da2df4634e
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-27 09:47:49 +0200

    how to print smthg

---
+++ how_to_print.py
@@ -0,0 +1,7 @@
+first_name = "Mala mi"
+age = 34
+#different ways of print
+print("hello", first_name, "!")
+print("hello" + first_name + "!")
+print(f"hello {first_name} !")
+print("Mam " + str(age) + " lata")
