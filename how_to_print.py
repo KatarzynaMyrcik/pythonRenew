4e9e1a7fdcf43dbe32d7acf5e7c1c5c914a36fca
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-27 09:54:13 +0200

    addition of math operations

--- how_to_print.py
+++ how_to_print.py
@@ -5,3 +5,18 @@ print("hello", first_name, "!")
 print("hello" + first_name + "!")
 print(f"hello {first_name} !")
 print("Mam " + str(age) + " lata")
+
+
+a = 2
+b = 1
+print('suma liczb wynosi', a + b)
+print('resta z dzielenia to', a%b)
+print('calkowita czesc dzielenia to', a // b)
+print('potege obliczysz tutaj', a ** b)
+
+"""
+tutaj wynikiem bedzie prawda lub falsz
+a == b
+a != b
+not a = b
+"""
