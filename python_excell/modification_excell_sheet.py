29ef41c97c263eac5f16cc3f9cea72bf24ac2a2f
KatarzynaMyrcik <74100123+KatarzynaMyrcik@users.noreply.github.com>
2023-04-28 15:01:44 +0200

    change into a function

--- python_excell/modification_excell_sheet.py
+++ python_excell/modification_excell_sheet.py
@@ -1,34 +1,35 @@
 from openpyxl import load_workbook
 from openpyxl.chart import BarChart, Reference
 
-wb = load_workbook("transactions.xlsx")
-print(wb.sheetnames)
-#ws = work Sheet
-ws = wb["Sheet1"]
-print(ws)
-cell = ws.cell(1,1)
-print(cell.value)
-print(f"max of rows it's: {ws.max_row}")
-
-for row in range(2, ws.max_row + 1):
-#	print(row)
-	cell = ws.cell(row,3)
-#	print(cell.value)
-	correted_price = cell.value * 0.9
-#	print(correted_price)
-	#how to add a values to new column
-	corrected_price_cell = ws.cell(row,4)
-	corrected_price_cell.value = correted_price
-ws.cell(1,ws.max_row).value = "corrected price"
-
-values = Reference(ws,
-		  min_row = 2,
-		  max_row = ws.max_row,
-		  min_col = 4,
-		  max_col = 4)
-
-chart = BarChart()
-chart.add_data(values)
-ws.add_chart(chart, "e2")
-
-wb.save("tranzakcje.xlsx")
+def process_workbook(filename):
+	wb = load_workbook("filename")
+	print(wb.sheetnames)
+	#ws = work Sheet
+	ws = wb["Sheet1"]
+	print(ws)
+	cell = ws.cell(1,1)
+	print(cell.value)
+	print(f"max of rows it's: {ws.max_row}")
+	
+	for row in range(2, ws.max_row + 1):
+	#	print(row)
+		cell = ws.cell(row,3)
+	#	print(cell.value)
+		correted_price = cell.value * 0.9
+	#	print(correted_price)
+		#how to add a values to new column
+		corrected_price_cell = ws.cell(row,4)
+		corrected_price_cell.value = correted_price
+	ws.cell(1,ws.max_row).value = "corrected price"
+	
+	values = Reference(ws,
+			  min_row = 2,
+			  max_row = ws.max_row,
+			  min_col = 4,
+			  max_col = 4)
+	
+	chart = BarChart()
+	chart.add_data(values)
+	ws.add_chart(chart, "e2")
+	
+	wb.save("filename")
